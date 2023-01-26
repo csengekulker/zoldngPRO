62bc05ed4c5285f517f82660a688565a5022f85b
csengekulker <k71608883429@ktch.hu>
2023-01-26 18:00:09 +0100

    landing page features and form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-import { FooterComponent } from './footer/footer.component'
+import { FooterComponent } from './footer/footer.component';
+import { BookingComponent } from './booking/booking.component';
+import { ServicesComponent } from './services/services.component';
+import { GalleryComponent } from './gallery/gallery.component'
 
 @NgModule({
   declarations: [
@@ -15,7 +18,10 @@ import { FooterComponent } from './footer/footer.component'
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    FooterComponent
+    FooterComponent,
+    BookingComponent,
+    ServicesComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
