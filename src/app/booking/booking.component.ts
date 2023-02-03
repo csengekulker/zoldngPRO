301b1792c02a6c68c9f4c44fe51522c64a91ee8c
csengekulker <k71608883429@ktch.hu>
2023-02-03 19:59:48 +0100

    booking form validation, refactor

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators as V } from '@angular/forms';
 import { ApiService } from '../shared/api.service';
 import { EmitterService } from '../emitter.service';
+import servicesJson from '../services/services.json'
 
 @Component({
   selector: 'app-booking',
@@ -10,8 +11,21 @@ import { EmitterService } from '../emitter.service';
 })
 export class BookingComponent implements OnInit{
 
-  bookings: any
-  bookingForm !: FormGroup
+  services = servicesJson.services
+  serviceNames: string[] = []
+  options: string[] = []
+
+  clientDetails!: Client
+
+  bookingForm = new FormGroup({
+    fullName: new FormControl('', V.required),
+    dob: new FormControl('', V.required),
+    email: new FormControl('', [V.required, V.email]),
+    phone: new FormControl('', V.required),
+    zipCode: new FormControl('', V.required),
+    city: new FormControl('', V.required),
+    address: new FormControl('', V.required)
+  })
 
   constructor(
     private api: ApiService,
@@ -19,19 +33,64 @@ export class BookingComponent implements OnInit{
     private emitter: EmitterService
   ) {}
 
-  formSubmit() {}
+  collectPersonalDetails() {
 
-  ngOnInit(): void {
-    this.bookingForm = this.formBuilder.group({
-      fullName: [''],
-      dob: [''],
-      email: [''],
-      phone: [''],
-      zipcode: [''],
-      city: [''],
-      address: ['']
+    const target = this.bookingForm.value
 
-    })
+    let fullName:string = target.fullName!
+    let dob:string = target.dob!
+    let email:string = target.email!
+    let phone:string = target.phone!
+    let zipCode = target.zipCode
+    let city = target.city
+    let address = target.address
+
+    let fullAddress = `${zipCode} ${city}, ${address}`
+
+    this.clientDetails = {
+      fullName,
+      dob: dob,
+      email,
+      phone,
+      fullAddress
+    }
+
+    console.log(this.clientDetails);
+    
+  }
+
+  optionChanged(event: any) {
+    console.log(event.target.value);
+    
+  }
+
+  onSubmit() {
+    this.collectPersonalDetails()
+  }
+
+  ngOnInit(): void { 
+    for (let i=0; i<this.services.length; i++) {
+      let serviceName:string = this.services[i].name
+      this.serviceNames.push(serviceName)
+      
+      for (let j=0; j<this.services[i].variants.length; j++) {
+
+        let option:string = this.services[i].variants[j].name
+        this.options.push(option)
+
+      }
+    }
+
+    console.log(this.serviceNames);
+    console.log(this.options);
+
   }
 }
 
+interface Client {
+  fullName: string,
+  dob: string,
+  email: string,
+  phone: string,
+  fullAddress: string
+}
