5b33211b994be2c75211dfa9c09a40a660aef23c
csengekulker <k71608883429@ktch.hu>
2023-04-20 14:07:58 +0200

    folders refactor, new component sidebar

---
+++ src/app/admin/main/sidebar/sidebar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent {
+
+  menu = [
+    {ref: 'clients', text: 'Vendegek' },
+    {ref: 'bookings', text: 'Foglalasok'},
+    {ref: 'appointments', text: 'Idopontok'},
+    {ref: 'services', text: 'Masszazsok'},
+    {ref: 'types', text: 'Tipusok'}
+  ]
+}
